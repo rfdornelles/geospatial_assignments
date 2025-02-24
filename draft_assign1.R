ce3d7d89ad2382671c9589c826ddff1473b12202
Rodrigo Dornelles <rodornelles@gmail.com>
2025-02-24 15:21:08 +0100

    Rodrigo's draft

---
+++ draft_assign1.R
@@ -0,0 +1,26 @@
+### Hertie School - Geospatial Analysis for Data Science 2025
+### Assignment 1
+### Due on 03 March 2025
+## Group: Luis Fernando Ramízes Ruiz, Montserrat Lopez Perez, Rodrigo Dornelles
+
+
+
+# •Create two points in the geographic CRS WGS 84 (EPSG: 4326) using the following latitude and longitude:
+#   •(A) longitude: 11.56362 , latitude: 48.12791
+#   •(B) longitude: -8.63953, latitude: 41.155140
+
+
+
+# 1. Compute the distance between these points in these four settings:
+#   
+#   (1)Great circle distance
+# (2)Project into the projected CRS Pseudo-Mercator WGS 84 (EPSG: 3857) and compute the Euclidean distance
+# (3)Project into the projected CRS LAEA (Lambert Azimuthal Equal Area) Europe (EPSG: 3035) and compute the Euclidean distance
+# (4) Project into the projected CRS ED 1950 Equidistant Conic Europe  Europe (ESRI:102031) and compute the Euclidean distance (authority ESRI – thus, set the crs parameter as crs=“ESRI: 102031“)
+# 
+
+
+# 2. Explain in a few sentences, why these results are different. Which is the least correct?
+#   
+#   
+#   (answer consists of 5 subtasks – 4 numbers and one textual explanation.)
